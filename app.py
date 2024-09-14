262f3d8898eefefe8b176aa3576200b6379321af
Gennadii Zdorikov <gena_zdorikov@icloud.com>
2024-09-14 17:40:57 +0300

    Start with sqlite3

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# about_me = {
-#     "name": "Вадим",
-#     "surname": "Шиховцов",
-#     "email": "vshihovcov@specialist.ru"
-#     }
-
-# @app.route("/about")
-# def about():
-#     return about_me
-
-# @app.route("/")
-# def hello_world():
-#     return "Hello, World!"
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 from typing import Any
 from flask import Flask, jsonify, request, g, abort
 from random import choice
@@ -194,3 +173,24 @@ def delete_quote(quote_id: int):
 
 if __name__ == "__main__":
    app.run(debug=True)
+
+
+   # from flask import Flask
+
+# app = Flask(__name__)
+
+# about_me = {
+#     "name": "Вадим",
+#     "surname": "Шиховцов",
+#     "email": "vshihovcov@specialist.ru"
+#     }
+
+# @app.route("/about")
+# def about():
+#     return about_me
+
+# @app.route("/")
+# def hello_world():
+#     return "Hello, World!"
+# if __name__ == "__main__":
+#     app.run(debug=True)
